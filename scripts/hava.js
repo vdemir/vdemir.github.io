2e02edfc631673193fd3445b31581da3fb60af58
Vedat Demir <vsahbaz@gmail.com>
2018-08-28 21:25:53 +0300

    2091

--- scripts/hava.js
+++ scripts/hava.js
@@ -1,12 +1,4 @@
 
-		if (typeof jQuery == 'undefined') {
-			document.write(unescape("%3Cscript src='/scripts/jquery.1.9.1.min.js' type='text/javascript'%3E%3C/script%3E"));
-		}
-	</script>
-
-	<script src="/scripts/openweather.min.js"></script>
-
-	<script>
 
 		$(function() {
 
