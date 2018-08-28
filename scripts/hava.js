f4aa9a065cbcd863e6bf63cf8b18c1492be55df0
Vedat Demir <vsahbaz@gmail.com>
2018-08-28 21:18:20 +0300

    2091

--- scripts/hava.js
+++ scripts/hava.js
@@ -1,6 +1,4 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-	<script>
 		if (typeof jQuery == 'undefined') {
 			document.write(unescape("%3Cscript src='/scripts/jquery.1.9.1.min.js' type='text/javascript'%3E%3C/script%3E"));
 		}
@@ -39,4 +37,3 @@
 
 		});
 
-	</script>
