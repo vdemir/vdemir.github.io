0c9f2107f6a2e9a9a75160264ecd3c62726296f0
Vedat Demir <vsahbaz@gmail.com>
2019-03-11 09:47:43 +0300

    2091

---
+++ assets/strproc (1).py
@@ -0,0 +1,35 @@
+Mehdi as. Tevratta YUSUF oğlu *MESİH olarak geçmektedir. Kova burcunda doğmuştur. Mehdi as. hicri 1400'de faaliyete başlar 1419'da 40'lı yaşlarında zuhur eder-aniden meşhur olur- 40 yıl, iman hakikatlerini anlattığı faaliyet yıllarıdır.
+
+*Mesih lakaptır elini süren mesh eden anlamındadır. İsa as. için kullanılmıştır.
+"2-LUK.3:23 İsa görevine başladığı zaman otuz yaşlarındaydı. Yusuf'un oğlu olarak biliniyordu. Yusuf da Eli oğlu, Yakup oğlu, İshak oğlu, İbrahim oğlu..."
+
+Kuranda Mehdi ile ilgili ayetler;
+Yasin Suresi 21. Ayet: "Sizden ücret istemeyenlere uyun, onlar *mehdilerdir." 
+*muhtedûn(e): hidayet bulmuş kimselerdir.
+
+Mehdi (as.)ın 1 yılı bizim 20 yılımıza denktir. Aradan 1440-260=1180  yıl geçmiş 72-13=59 yıl eder. 12. İmam* o zaman 5 yaşlarında olsa şimdiki yaşı 63-64 olur. *İmam hicri 255'te doğdu, en son 260'ta 5 yaşında görüldü.
+
+"(8) Allah’ın nurunu ağızlarıyle söndürmek istiyorlar ve Allah nurunu tamamlayacaktır ve şayet kafirler hoşlanmasa da •61. Saff Sûresi•"
+
+Arabistan Takvimine Göre:
+Hicri Yılbaşı 1 Muharrem 1400  tarihinin miladi karşılığı 20 Kasım 1979 Salı
+Miladi 40 Yıl Faaliyet; 
+20 Kasım 2019 Çarşamba tarihinin hicri karşılığı 23 Rebiülevvel 1441
+
+Atatürk'ün karşı olduğu şey din değildi, yobazlığa ve bağnazlığa karşıydı. Atatürk dindardı; Kuran tefsiri yaptırdı, İmam Hatipleri açtı, Diyanet İşlerini kurdu, Kuran'ı yanından hiç ayırmazdı.
+Tayyip imam hatipler açtı, başörtüsünü serbet bıraktı, dini özgürlükler getirdi. Kuran okur, talabesidir.
+
+Atatürk Saltanat Mehdisi, Bediüzzaman Said Nursi Diyanet Mehdisi ve Tayyip Siyaset Mehdisidir. Kaim Ahirzaman Mehdisi Ayrıdır. Bu Üç Görevi Beraber Yapacaktır. Hadislere Göre Orta Boyludur. Bütün dünya halkları,  insanlar Mehdiyi bekliyor. Her geçen gün bu istek, bu haykırış daha çok duyuluyor ve duyulacak. Başka yolu yok. Kaim Mehdi Gelecek. Sultan Gelecek. Dünya Adalet ve barış ile dolacak. Mehdi Melek derecesinde Sevgi öğretmenidir. Bekle Göreceksin. İnŞaALLAH.
+
+Cemal Kaşıkçı - Nefsi Zekiyye
+Erbakan ve Tayyip - Yemaniler as.
+
+"Hacc-ı ekber Rasulüllah`ın veda haccıdır ve o gün bir Cuma günü idi"
+
+Bu sene 2012-10 Zihicce Haccül ekberdir. Vaktin sahibi Haccül ekberde bilinir. Bu Haccül ekber geçerse ondan sonra yedi sene geçmesi gerekir. Buna dünyanın tahammülü yok.  Bu sene 10 Zihicce Arafat vakfesi Cuma günü olur. Haccül ekber 70 hac değerindedir. Ümid ederim vaktin sahibi çıksın. 2020-10 Zihicce 1441 Cuma
+
+Arabistan Takvimine Göre: Kurban Bayramı 1. Gün
+Hicri 10 Zilhicce 1433  tarihinin miladi karşılığı 26 Ekim 2012 Cuma
+Hicri 10 Zilhicce 1441  tarihinin miladi karşılığı 31 Temmuz 2020 Cuma
+.
+.﻿
