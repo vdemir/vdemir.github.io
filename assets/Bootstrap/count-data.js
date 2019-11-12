54abe47a5d876bd768c6f8b46e5af5d52c34535f
vdemir <vsahbaz@gmail.com>
2019-11-12 19:45:16 +0300

    commit

--- assets/Bootstrap/count-data.js
+++ assets/Bootstrap/count-data.js
@@ -1,5 +1,5 @@
 var DISQUSWIDGETS;
 
 if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"https:\/\/plugins.krajee.com\/file-resumable-uploads-demo","comments":0}]});
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"https:\/\/plugins.krajee.com\/file-krajee-explorer-demo","comments":15}]});
 }
